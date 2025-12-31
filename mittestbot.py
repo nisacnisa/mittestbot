1946c890da6aef8109da797748f4bd3548d33e1f
nisacnisa <Nisanjrealtor@gmail.com>
2025-12-31 16:21:42 -0500

    Update mittestbot.py

--- mittestbot.py
+++ mittestbot.py
@@ -1,1 +1,23 @@
+from dotenv import load_dotenv
+import discord
+import os
 
+#load environment variables from .env file
+load_dotenv()
+
+#setup intents
+intents = discord.Intents.default()
+intents.message_content = True #ensure that your bot cna read message content
+
+client = discord.Client (intents=intents)
+
+@clientevent
+async def on_ready():
+  print('We have logged in as {0.user}'. format (client)
+        @clientevent
+        async def on_message (message):
+          if message.author == clientuser:
+            return
+        
+
+    
